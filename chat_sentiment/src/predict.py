25602aa59dd9f977eec71bd20101d68e177334fc
Kirit Ranjan <kiritranjan7@gmail.com>
2026-05-03 02:59:42 +0530

    fix: use os to load model in predict.py

--- chat_sentiment/src/predict.py
+++ chat_sentiment/src/predict.py
@@ -1,7 +1,12 @@
 import joblib
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "..", "model", "model.pkl")
+
+model = joblib.load(model_path)
 
 def predict(text):
-    model = joblib.load('../model/model.pkl')
 
     result = model.predict([text])[0]
 
